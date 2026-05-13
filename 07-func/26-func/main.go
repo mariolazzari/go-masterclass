111d0636fc086993f5d9f0db40a2da9dd7b73cca
Mario Lazzari <mario.lazzari@gmail.com>
2026-05-13 22:03:14 +0200

    Done lesson 26

--- 07-func/26-func/main.go
+++ 07-func/26-func/main.go
@@ -1,5 +1,31 @@
 package main
 
+import "fmt"
+
+func greet(name string) {
+	fmt.Printf("Ciao %s\n", name)
+}
+
+func add(a, b int) int {
+	return a + b
+}
+
+func area(width, height float64) float64 {
+	if width < 0 || height < 0 {
+		return 0
+	}
+
+	return width * height
+}
+
 func main() {
+	greet("Mario")
+
+	a := 1
+	b := 2
+	c := add(a, b)
+	fmt.Printf("%d + %d = %d\n", a, b, c)
+
+	fmt.Println("area = ", area(4, 5))
 
 }
